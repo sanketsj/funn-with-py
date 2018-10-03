33d0ee8e744073ef20034f1879b4eec58c909a75
sanketsj <40729436+sanketsj@users.noreply.github.com>
2018-10-04 00:35:15 +0530

    Update GfAutoMessageSender.py

--- GfAutoMessageSender.py
+++ GfAutoMessageSender.py
@@ -6,7 +6,7 @@ time.sleep(5)
 for i in range(0,500):#how many times you wont to send
 	pyautogui.moveTo(900, 646) #cordinates are change heare
 	pyautogui.click()
-	pyautogui.typewrite(['h','a','p','p','y',' ','b','i','r','t','h','d','a','y'])     
+	pyautogui.typewrite(['h','a','p','p','y',' ','b','i','r','t','h','d','a','y']) #msg what you wont to send your bee    
 	pyautogui.press('enter') 
 	g=random.uniform(0, 0.9)
 	print(g)
