79cb5083e6e18b069ed4b7c8dc1cd93dce4c28fe
sanketsj <40729436+sanketsj@users.noreply.github.com>
2018-10-04 00:34:11 +0530

    Create GfAutoMessageSender.py

---
+++ GfAutoMessageSender.py
@@ -0,0 +1,14 @@
+import pyautogui 
+import time
+import random
+pyautogui.FAILSAFE = True
+time.sleep(5)
+for i in range(0,500):#how many times you wont to send
+	pyautogui.moveTo(900, 646) #cordinates are change heare
+	pyautogui.click()
+	pyautogui.typewrite(['h','a','p','p','y',' ','b','i','r','t','h','d','a','y'])     
+	pyautogui.press('enter') 
+	g=random.uniform(0, 0.9)
+	print(g)
+	time.sleep(g)
+
